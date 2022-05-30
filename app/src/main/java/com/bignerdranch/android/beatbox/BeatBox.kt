8291aded7f113a1176705d5459b1a90f0a7240f1
LukaszTheLittle <lukis.maly@gmail.com>
2022-05-30 17:43:38 +0200

    Added `loadSounds` method to look up for assets

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
@@ -1,8 +1,20 @@
 package com.bignerdranch.android.beatbox
 
 import android.content.res.AssetManager
+import android.util.Log
 
 class BeatBox(private val assets: AssetManager) {
+
+    fun loadSounds(): List<String> {
+        try {
+            val soundNames = assets.list(SOUNDS_FOLDER)!!
+            Log.d(TAG, "Found ${soundNames.size} sounds")
+            return soundNames.asList()
+        } catch (e: Exception) {
+            Log.e(TAG, "Could not list assets", e)
+            return emptyList()
+        }
+    }
 
     companion object {
         private const val TAG = "BeatBox"
