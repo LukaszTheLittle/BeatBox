9f3e2afe9250cebd39a6be6c58a49475f8127138
LukaszTheLittle <lukis.maly@gmail.com>
2022-05-30 17:37:14 +0200

    Created `BeatBox` class to manage assets

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.beatbox
+
+import android.content.res.AssetManager
+
+class BeatBox(private val assets: AssetManager) {
+
+    companion object {
+        private const val TAG = "BeatBox"
+        private const val SOUNDS_FOLDER = "sample_sounds"
+    }
+}
+
