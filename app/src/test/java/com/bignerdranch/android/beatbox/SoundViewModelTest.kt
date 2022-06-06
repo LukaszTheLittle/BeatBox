1c7a8cdf2dadc03ae03b01404e92a61d8020d41e
LukaszTheLittle <lukis.maly@gmail.com>
2022-06-06 23:57:51 +0200

    Created a mock BeatBox

--- app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.kt
+++ app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.kt
@@ -3,14 +3,17 @@ package com.bignerdranch.android.beatbox
 import org.junit.Assert.assertSame
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito.mock
 
 class SoundViewModelTest {
 
+    private lateinit var beatBox: BeatBox
     private lateinit var sound: Sound
     private lateinit var subject: SoundViewModel
 
     @Before
     fun setUp() {
+        beatBox = mock(BeatBox::class.java)
         sound = Sound("assetPath")
         subject = SoundViewModel()
         subject.sound = sound
