a0f633a840615979c6bbaba2e6a83dddd0541e48
LukaszTheLittle <lukis.maly@gmail.com>
2022-06-06 22:34:03 +0200

    Created a `SoundViewModel` test subject

--- app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.kt
+++ app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.kt
@@ -1,12 +1,16 @@
 package com.bignerdranch.android.beatbox
 
-import org.junit.Assert.*
-
 import org.junit.Before
 
 class SoundViewModelTest {
 
+    private lateinit var sound: Sound
+    private lateinit var subject: SoundViewModel
+
     @Before
     fun setUp() {
+        sound = Sound("assetPath")
+        subject = SoundViewModel()
+        subject.sound = sound
     }
 }
