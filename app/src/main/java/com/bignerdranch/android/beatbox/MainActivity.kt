a8a547b53885937726f1e63a3788661b4938d712
LukaszTheLittle <lukis.maly@gmail.com>
2022-06-08 23:30:24 +0200

    Deleted unused import directives

--- app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
@@ -2,12 +2,9 @@ package com.bignerdranch.android.beatbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.beatbox.databinding.ActivityMainBinding
-import com.bignerdranch.android.beatbox.databinding.ListItemSoundBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,33 +25,5 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         soundAdapter.beatBoxRelease()
         super.onDestroy()
-    }
-
-    private inner class SoundAdapter(
-        private val beatBox: BeatBox
-        ):
-        RecyclerView.Adapter<SoundHolder>() {
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
-                SoundHolder {
-            val binding = DataBindingUtil.inflate<ListItemSoundBinding>(
-                layoutInflater,
-                R.layout.list_item_sound,
-                parent,
-                false
-            )
-            return SoundHolder(binding, beatBox)
-        }
-
-        override fun onBindViewHolder(holder: SoundHolder, position: Int) {
-            val sound = beatBox.sounds[position]
-            holder.bind(sound)
-        }
-
-        override fun getItemCount() = beatBox.sounds.size
-
-        fun beatBoxRelease() {
-            beatBox.release()
-        }
     }
 }
