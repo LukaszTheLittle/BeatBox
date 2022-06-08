d0e4fef7c90ec4a58e228cad8a3e1262fec79f41
Pantkowsky <a.pantkowski@gmail.com>
2022-06-08 22:41:33 +0200

    some cleanups

--- app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
@@ -2,14 +2,13 @@ package com.bignerdranch.android.beatbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.beatbox.databinding.ActivityMainBinding
-import com.bignerdranch.android.beatbox.databinding.ListItemSoundBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val soundAdapter: SoundAdapter = SoundAdapter(BeatBox(assets))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,52 +18,12 @@ class MainActivity : AppCompatActivity() {
 
         binding.recyclerView.apply {
             layoutManager = GridLayoutManager(context, 3)
-            adapter = SoundAdapter(BeatBox(assets).sounds)
+            adapter = soundAdapter
         }
     }
 
     override fun onDestroy() {
+        soundAdapter.onDestroy()
         super.onDestroy()
-        BeatBox(assets).release()
-    }
-
-    private inner class SoundHolder(
-        private val binding: ListItemSoundBinding,
-        beatBox: BeatBox
-        ):
-            RecyclerView.ViewHolder(binding.root) {
-
-        init {
-            binding.viewModel = SoundViewModel(beatBox)
-        }
-
-        fun bind(sound: Sound) {
-            binding.apply {
-                viewModel?.sound = sound
-                executePendingBindings()
-            }
-        }
-    }
-
-    private inner class SoundAdapter(private val sounds: List<Sound>):
-        RecyclerView.Adapter<SoundHolder>() {
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
-                SoundHolder {
-            val binding = DataBindingUtil.inflate<ListItemSoundBinding>(
-                layoutInflater,
-                R.layout.list_item_sound,
-                parent,
-                false
-            )
-            return SoundHolder(binding, BeatBox(assets))
-        }
-
-        override fun onBindViewHolder(holder: SoundHolder, position: Int) {
-            val sound = sounds[position]
-            holder.bind(sound)
-        }
-
-        override fun getItemCount() = sounds.size
     }
 }
---
+++ app/src/main/java/com/bignerdranch/android/beatbox/SoundHolder.kt
@@ -0,0 +1,21 @@
+package com.bignerdranch.android.beatbox
+
+import androidx.recyclerview.widget.RecyclerView
+import com.bignerdranch.android.beatbox.databinding.ListItemSoundBinding
+
+class SoundHolder(
+    private val binding: ListItemSoundBinding,
+    beatBox: BeatBox
+) : RecyclerView.ViewHolder(binding.root) {
+
+    init {
+        binding.viewModel = SoundViewModel(beatBox)
+    }
+
+    fun bind(sound: Sound) {
+        binding.apply {
+            viewModel?.sound = sound
+            executePendingBindings()
+        }
+    }
+}
