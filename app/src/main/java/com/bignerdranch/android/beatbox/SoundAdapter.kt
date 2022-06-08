e2f9b4a1d96a21bb9a1c034ded8f84c5c13dbca8
LukaszTheLittle <lukis.maly@gmail.com>
2022-06-08 23:38:46 +0200

    Moved `SoundAdapter` class to another file

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/SoundAdapter.kt
@@ -0,0 +1,33 @@
+package com.bignerdranch.android.beatbox
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bignerdranch.android.beatbox.databinding.ListItemSoundBinding
+
+class SoundAdapter(private val beatBox: BeatBox):
+    RecyclerView.Adapter<SoundHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            SoundHolder {
+        val binding = DataBindingUtil.inflate<ListItemSoundBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.list_item_sound,
+            parent,
+            false
+        )
+        return SoundHolder(binding, beatBox)
+    }
+
+    override fun onBindViewHolder(holder: SoundHolder, position: Int) {
+        val sound = beatBox.sounds[position]
+        holder.bind(sound)
+    }
+
+    override fun getItemCount() = beatBox.sounds.size
+
+    fun beatBoxRelease() {
+        beatBox.release()
+    }
+}
